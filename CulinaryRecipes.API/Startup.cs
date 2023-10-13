e64d44e5ac38a4c35e6b86b820fa53fbf3239368
Pawel W <pswegr@hotmail.com>
2023-10-13 02:04:20 +0200

    create startup class

---
+++ CulinaryRecipes.API/Startup.cs
@@ -0,0 +1,15 @@
+﻿namespace CulinaryRecipes.API
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Minimal or no actual configuration needed.
+        }
+
+        public void Configure(IApplicationBuilder app, IConfiguration configuration)
+        {
+            // Minimal or no actual configuration needed.
+        }
+    }
+}
